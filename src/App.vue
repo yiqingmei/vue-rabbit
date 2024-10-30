6c6df64e9f0b03f977230f92723918c8376fa061
yiqingmei <yiqing.mei.life@gmail.com>
2024-10-31 01:12:53 +0800

    一级二级路由

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { RouterView } from 'vue-router';
+
 
 </script>
 
 <template>
-  <el-button type="primary">Primary</el-button>
+  <RouterView/>
 </template>
 
 <style scoped>
