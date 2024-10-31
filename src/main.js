7fb7a113aafa2b46def4668adeecdb46b186c7f8
yiqingmei <yiqing.mei.life@gmail.com>
2024-11-01 00:37:39 +0800

    引入静态文件

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+import '@/styles/common.scss'
+
 import { getCategoryAPI } from '@/apis/testAPI'
 getCategoryAPI().then(res =>
   console.log(res)
 )
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
 
