f05b28483abc8125d449d7a19b98d656cb8d563f
yiqingmei <yiqing.mei.life@gmail.com>
2024-11-02 13:55:02 +0800

    激活状态显示，分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,31 +3,32 @@ import { getCategoryAPI } from '@/apis/category';
 import { onMounted, ref, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 import { getBannerAPI } from '@/apis/home';
+import GoodItem from '../Home/components/GoodItem.vue';
 
 const categoryData = ref({})
 const route = useRoute()
-const getCategory = async () =>{
-    const res = await getCategoryAPI(route.params.id)
-    console.log(res)
-    categoryData.value = res.result
+const getCategory = async () => {
+  const res = await getCategoryAPI(route.params.id)
+  console.log(res)
+  categoryData.value = res.result
 }
 
 const bannerList = ref([])
 
 const getBanner = async () => {
-    const res = await getBannerAPI({
-      distributionSite:'2'
-    })
-    console.log(res)
-    bannerList.value = res.result
+  const res = await getBannerAPI({
+    distributionSite: '2'
+  })
+  console.log(res)
+  bannerList.value = res.result
 }
 
 onMounted(() => {
-    getBanner()
+  getBanner()
 })
 
-watchEffect(() =>{
-    getCategory()
+watchEffect(() => {
+  getCategory()
 })
 </script>
 
@@ -38,17 +39,36 @@ watchEffect(() =>{
       <div class="bread-container">
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>{{categoryData.name}}</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <!-- 轮播图 -->
       <div class="home-banner">
         <el-carousel height="500px">
-            <el-carousel-item v-for="item in bannerList" :key="item.id">
-                <img :src="item.imgUrl" alt="">
-            </el-carousel-item>
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+          </el-carousel-item>
         </el-carousel>
-    </div>
+      </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -132,14 +152,15 @@ watchEffect(() =>{
     padding: 25px 0;
   }
 }
-.home-banner {
-    width: 1240px;
-    height: 500px;
-    margin: 0 auto;
 
-    img {
-        width: 100%;
-        height: 500px;
-    }
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
 }
 </style>
