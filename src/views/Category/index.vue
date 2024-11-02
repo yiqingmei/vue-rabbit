459136f90562901eba708cef530712684cf681ba
yiqingmei <yiqing.mei.life@gmail.com>
2024-11-02 13:41:19 +0800

    home轮播图

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,7 @@
 import { getCategoryAPI } from '@/apis/category';
 import { onMounted, ref, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
+import { getBannerAPI } from '@/apis/home';
 
 const categoryData = ref({})
 const route = useRoute()
@@ -10,6 +11,20 @@ const getCategory = async () =>{
     console.log(res)
     categoryData.value = res.result
 }
+
+const bannerList = ref([])
+
+const getBanner = async () => {
+    const res = await getBannerAPI({
+      distributionSite:'2'
+    })
+    console.log(res)
+    bannerList.value = res.result
+}
+
+onMounted(() => {
+    getBanner()
+})
 
 watchEffect(() =>{
     getCategory()
@@ -26,6 +41,14 @@ watchEffect(() =>{
           <el-breadcrumb-item>{{categoryData.name}}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+            <el-carousel-item v-for="item in bannerList" :key="item.id">
+                <img :src="item.imgUrl" alt="">
+            </el-carousel-item>
+        </el-carousel>
+    </div>
     </div>
   </div>
 </template>
@@ -108,5 +131,15 @@ watchEffect(() =>{
   .bread-container {
     padding: 25px 0;
   }
+}
+.home-banner {
+    width: 1240px;
+    height: 500px;
+    margin: 0 auto;
+
+    img {
+        width: 100%;
+        height: 500px;
+    }
 }
 </style>
